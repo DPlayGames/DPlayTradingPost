3cc996ca45e711868607cd5cb9c5dbe4884d39ed
Hanul <hanul@hanul.me>
2019-06-05 17:22:12 +0900

    거래 정보 추가

--- DPlayTradingPostInterface.sol
+++ DPlayTradingPostInterface.sol
@@ -2,11 +2,37 @@ pragma solidity ^0.5.1;
 
 interface DPlayTradingPostInterface {
 	
+	// 거래 정보
+	struct Trade {
+		string title;
+		string description;
+		string price;
+		
+		// 자원(ERC-20)은 최대 4개까지 지정 가능합니다.
+		address resource1Address;
+		uint resource1Amount;
+		address resource2Address;
+		uint resource2Amount;
+		address resource3Address;
+		uint resource3Amount;
+		address resource4Address;
+		uint resource4Amount;
+		
+		// 자원(ERC-721)은 최대 9개까지 지정 가능합니다.
+		address item1Address;
+		address item2Address;
+		address item3Address;
+		address item4Address;
+		address item5Address;
+		address item6Address;
+		address item7Address;
+		address item8Address;
+		address item9Address;
+	}
+	
 	// 물품 등록
+	// sell
 	
 	// 물품 구매
-	
-	// 경매 등록
-	
-	// 경매 참가
+	// buy
 }
