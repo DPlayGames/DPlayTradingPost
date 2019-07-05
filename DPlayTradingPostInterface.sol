b268849e13e0765222b207e153e0a334a6dba64b
codingcomposer <43369346+codingcomposer@users.noreply.github.com>
2019-07-05 11:33:58 +0900

    Update DPlayTradingPostInterface.sol

--- DPlayTradingPostInterface.sol
+++ DPlayTradingPostInterface.sol
@@ -22,6 +22,7 @@ interface DPlayTradingPostInterface {
 		uint		createTime;
 	}
 	
+	// Unique item sale info
 	// 유니크 아이템 판매 정보
 	struct UniqueItemSale {
 		address		seller;
@@ -36,6 +37,7 @@ interface DPlayTradingPostInterface {
 	// 아이템 판매 횟수를 반환합니다.
 	function getItemSaleCount() external view returns (uint);
 	
+	// Returns the number of unique item sales.
 	// 유니크 아이템 판매 횟수를 반환합니다.
 	function getUniqueItemSaleCount() external view returns (uint);
 	
@@ -48,6 +50,7 @@ interface DPlayTradingPostInterface {
 		string calldata		description
 	) external returns (uint);
 	
+	// Sells unique items.
 	// 유니크 아이템을 판매합니다.
 	function sellUniqueItem(
 		address[] calldata	itemAddresses,
@@ -60,6 +63,7 @@ interface DPlayTradingPostInterface {
 	// 특정 주소가 아이템 판매자인지 확인합니다.
 	function checkIsItemSeller(address addr, uint saleId) external view returns (bool);
 	
+	// Checks if the given address is the unique item seller.
 	// 특정 주소가 유니크 아이템 판매자인지 확인합니다.
 	function checkIsUniqueItemSeller(address addr, uint saleId) external view returns (bool);
 	
@@ -67,6 +71,7 @@ interface DPlayTradingPostInterface {
 	// 특정 판매자가 판매중인 아이템 판매 ID들을 가져옵니다.
 	function getItemSaleIds(address seller) external view returns (uint[] memory);
 	
+	// Gets the sale IDs of the unique items sold by the given seller.
 	// 특정 판매자가 판매중인 유니크 아이템 판매 ID들을 가져옵니다.
 	function getUniqueItemSaleIds(address seller) external view returns (uint[] memory);
 	
@@ -81,6 +86,7 @@ interface DPlayTradingPostInterface {
 		uint createTime
 	);
 	
+	// Returns unique item sale info.
 	// 유니크 아이템 판매 정보를 반환합니다.
 	function getUniqueItemSaleInfo(uint saleId) external view returns (
 		address seller,
@@ -95,6 +101,7 @@ interface DPlayTradingPostInterface {
 	// 아이템 판매를 취소합니다.
 	function cancelItemSale(uint saleId) external;
 	
+	// Cancels a unique item sale.
 	// 유니크 아이템 판매를 취소합니다.
 	function cancelUniqueItemSale(uint saleId) external;
 	
@@ -102,6 +109,7 @@ interface DPlayTradingPostInterface {
 	// 아이템을 구매합니다.
 	function buyItem(uint saleId) payable external;
 	
+	// Buys unique items.
 	// 유니크 아이템을 구매합니다.
 	function buyUniqueItem(uint saleId) payable external;
 }
